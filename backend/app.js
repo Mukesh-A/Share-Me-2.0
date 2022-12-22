1bfe69dddee0448049ba24f2eaa21e5f87b9fc0f
Mukesh-a <mukeshabhi08@gmail.com>
2022-12-22 20:31:57 +0530

    Deploy

--- backend/app.js
+++ backend/app.js
@@ -36,12 +36,12 @@ mongoose
     //   });
     // }
     if (process.env.NODE_ENV === "production") {
-      app.use(express.static(path.join(__dirname, "../frontend", "build")));
+      app.use(express.static(path.join(__dirname, "frontend", "build")));
       app.get("/*", (req, res) => {
         res.sendFile(
-          path.join(__dirname, "../frontend", "build", "index.html")
+          path.join(__dirname, "frontend", "build", "index.html")
         );
-        res.write(path.join(__dirname, "../frontend", "build", "index.html"))
+        // res.write(path.join(__dirname, "../frontend", "build", "index.html"))
       });
     }
 
