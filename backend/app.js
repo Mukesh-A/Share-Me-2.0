c31d6ee667cb76272f54f2fc52f94edaea3e948b
Mukesh-a <mukeshabhi08@gmail.com>
2022-12-22 21:10:16 +0530

    Deploy

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ mongoose
   .then(() => {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("/*", function (_, res) {
+    app.get("../*", function (_, res) {
       res.sendFile(
         path.join(__dirname, "../frontend/build/index.html"),
         function (err) {
