bf65fe0586e96813052e9bb8ba63ecae4f43e9ab
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2022-12-22 17:50:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -31,3 +31,10 @@ mongoose
   )
   .catch((err) => console.log(err));
 
+// static files (build of your frontend)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
